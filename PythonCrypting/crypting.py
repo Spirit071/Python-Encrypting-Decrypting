a28e276703f297bb87780b01dc31dcccab3c1f18
Spirit <96442875+Spirit071@users.noreply.github.com>
2022-08-11 17:30:54 +0300

    Update crypting.py

--- PythonCrypting/crypting.py
+++ PythonCrypting/crypting.py
@@ -9,7 +9,8 @@ def get_random_number():
 # this function is used to delete earlier json data, and recieving new data
 def clearJson():
     with open('randoms.json', 'w') as F:
-            json.dump(None, F)
+            json.dump("", F)
+            
 
 def rndNumListCreator(s):
     rndNumList = []
@@ -28,6 +29,7 @@ def rndNumListCreator(s):
 def decrypt(toDecrypt):  # the 'toDecrypt' string is an encrypted message
     # load data from json and prepare it for decryption
    with open('randoms.json', 'r') as openfile:
+        try:
             # read the list of random numbers from the file
             data = json.load(openfile)
             index = 0
@@ -37,6 +39,10 @@ def decrypt(toDecrypt):  # the 'toDecrypt' string is an encrypted message
                 s+=a
                 index+=1
             print("The decrypted message is: {0}".format(s))
+            
+        except Exception:
+            print('An error has accured. Please try again\n')
+            main(var = False)
 
 def encrypt(q):
     s = ''
@@ -48,39 +54,39 @@ def encrypt(q):
             a = chr(ord(char) + int(n[index])) # turn the ASCII into an actual text(char)
             s = s + a
             index = index + 1
-        print(f' The encrypted message is: {s}')
+        print(f'The encrypted message is: {s}')
 
 
-def main():
+def main(var):
     action = input("Encrypt(e) or decrypt(d)? ").lower()  # get the action
-    var = 0
+    var = False
     '''
     encrypting the message
     '''
     if action == 'e':
         string = input("Please enter a message to encrypt: ")      
         rev = ''
-        var = 1
+        var = True
         for char in string[::-1]:
             rev = rev + char
 # see if we have a palindromic message
         if rev == string:
             print('Error! Palindromic message detected!')
-            main()
+            main(var = False)
             # if not then encrypt
         else:
             encrypt(string)
 # if we want to decrypt the message then decrypt it
     if action == 'd':
-        var = 1
+        var = True
         string = input("Please enter a message to decrypt: ")   
         decrypt(string)
         clearJson()
     # if another action provided and we don't have another function running, rerun the script
     else:
-        if var != 1:
-            main()
+        if not var:
+            main(True)
+        
 
 if __name__ == '__main__':
-    var = 1
-    main()
+    main(var = True)
